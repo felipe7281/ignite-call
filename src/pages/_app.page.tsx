f8119c8201075a24d5c76777f3fab45104c8f6b3
Felipe Ferreira <felizpeu@MacBook-Pro-de-vitor-2.local>
2023-02-21 09:35:12 -0300

    first commit

---
+++ src/pages/_app.page.tsx
@@ -0,0 +1,16 @@
+import type { AppProps } from 'next/app'
+import { globalStyles } from '../styles/global'
+import { SessionProvider } from 'next-auth/react'
+
+globalStyles() // Ele fica aqui fora pq só precisa renderizar uma vez, se colocar dentro da função vai carregar sempre que atualizar.
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
+}
